0765def6cb66cd7e5ce7b83b30d1e129a6e46e2c
pival81 <pival801@gmail.com>
2020-12-16 18:27:44 +0100

    fixed ikonli issues with shading, whatever that is

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module com.pival81.Client {
     requires javafx.fxml;
     requires com.google.gson;
     requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.fontawesome5;
 
     opens com.pival81.Client.Controllers to javafx.fxml;
     exports com.pival81.Client;
